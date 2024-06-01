57be46fd68baac5332c5ea82681fb14af250772b
Marco Bonici <bonici.marco@gmail.com>
2024-06-01 18:56:34 -0400

    Improving performance

--- src/PlanckLite.jl
+++ src/PlanckLite.jl
@@ -1,6 +1,7 @@
 module PlanckLite
 
 using Artifacts
+using ChainRulesCore
 using LinearAlgebra
 using NPZ
 
@@ -41,5 +42,38 @@ function bin_Cℓ(Cltt::AbstractArray{T}, Clte, Clee) where {T}
 
     return vcat(Cltt_bin, Clte_bin, Clee_bin)
 end
+
+function bin_Cℓ(Cl::AbstractArray{T}, nbin, blmin, plmin, ellmin, blmax, bin_W) where {T}
+    #Cl_bin = zeros(T, nbin)
+
+    #for i in 1:nbin
+    #    Cl_bin[i] = LinearAlgebra.dot(Cl[blmin[i]+plmin-ellmin+1:blmax[i]+plmin+1-ellmin],bin_W[blmin[i]+1:blmax[i]+1])
+    #end
+
+    return [LinearAlgebra.dot(Cl[blmin[i]+plmin-ellmin+1:blmax[i]+plmin+1-ellmin],bin_W[blmin[i]+1:blmax[i]+1]) for i in 1:nbin]
+end
+
+function new_bin_Cℓ(Cltt::AbstractArray{T}, Clte, Clee) where {T}
+    Cltt_bin = bin_Cℓ(Cltt, nbintt, blmin_TT, plmin_TT, ellmin, blmax_TT, bin_w_TT)
+    Clte_bin = bin_Cℓ(Clte, nbinte, blmin, plmin, ellmin, blmax, bin_w)
+    Clee_bin = bin_Cℓ(Clee, nbinee, blmin, plmin, ellmin, blmax, bin_w)
+
+    return vcat(Cltt_bin, Clte_bin, Clee_bin)
+end
+
+
+#function ChainRulesCore.rrule(::typeof(bin_Cℓ), Cl, nbin, blmin, plmin, ellmin, blmax, bin_W)
+#    Y = bin_Cℓ(Cl, nbin, blmin, plmin, ellmin, blmax, bin_W)
+#    function bin_Cℓ_pullback(Ȳ)
+ #       ∂Cl_bin = zeros(nbin)#
+#
+#        for i in 1:nbin
+#            ∂Cl_bin[i] = LinearAlgebra.dot(Ȳ[blmin[i]+plmin-ellmin+1:blmax[i]+plmin+1-ellmin],bin_W[blmin[i]+1:blmax[i]+1])
+#        end
+#        return NoTangent(), ∂Cl_bin, NoTangent(), NoTangent(), NoTangent(), NoTangent(), NoTangent(), NoTangent()
+#    end#
+#
+#    return Y, bin_Cℓ_pullback
+#end
 
 end # module PlanckLite
